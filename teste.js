09d7300324bd8d1e3c4cdd0194b44a6072fc6107
AugustoFrancoDev <98070772+AugustoFrancoDev@users.noreply.github.com>
2022-01-25 11:36:13 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -1,20 +1,49 @@
-const mobileMenu = document.querySelector(".menu-mobile"),
-navList = document.querySelector(".cabecalho-menu"); 
-navLinks = document.querySelectorAll(".cabecalho-menu-item");
-activeClass = "active";
-let menuAtivo = false
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+ this.mobileMenu = document.querySelector(mobileMenu),
+ this.navList = document.querySelector(navList); 
+ this.navLinks = document.querySelectorAll(navLinks);
+ this.activeClass = "active";
 
+ this.handleClick = this.handleClick.bind(this);
+  }
 
+  animateLinks() {
+      this.navLinks.forEach((link, index) => {
+          link.style.animation
+          ?(link.style.animation = "")
+          :(link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+      });
+  } /* "index" é a primeira posição no "Each" até a última */
 
-function clicarMenu() {
-    if (menuAtivo == false) {navList.classList.toggle(activeClass);        
-    } 
-    else {navList.classList.remove(activeClass);
-    }
+  handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+  }
+
+  addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick)
+  }
+
+  init() {
+      if (this.mobileMenu) {
+          this.addClickEvent(); 
+      }
+      return this;
+  } /* Inicia a função se mobileMenu existir no documento */
 }
 
-function inverterValor() {
-    return menuAtivo = !menuAtivo    
-}
+const mobileNavbar = new MobileNavbar (
+    ".menu-mobile",
+    ".cabecalho-menu",
+    ".cabecalho-menu-item"
+);
 
-mobileMenu.addEventListener("click", () => (clicarMenu(), inverterValor()))
+mobileNavbar.init()
+
+    
+
+   
+   
+   
